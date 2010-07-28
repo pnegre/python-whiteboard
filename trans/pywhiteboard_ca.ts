82c4956ffcff64eabe93d2f3310499cee02bc388
pnegre <pnegre@andromeda.lan>
2010-07-28 20:00:26 +0200

    Small fixes in catalan translation

--- trans/pywhiteboard_ca.ts
+++ trans/pywhiteboard_ca.ts
@@ -1,103 +1,169 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ca_ES">
+<context>
+    <name>CalibrateDialog</name>
+    <message>
+        <location filename="calibration.py" line="292"/>
+        <source>Push UP/DOWN to alter the crosses&apos; position</source>
+        <translation type="unfinished">Pitja les tecles AMUNT/AVALL del cursor per alterar la posició de les creus</translation>
+    </message>
+</context>
+<context>
+    <name>CalibrateDialog2</name>
+    <message>
+        <location filename="calibration.py" line="142"/>
+        <source>TOP-LEFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calibration.py" line="143"/>
+        <source>TOP-RIGHT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calibration.py" line="144"/>
+        <source>BOTTOM-RIGHT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calibration.py" line="145"/>
+        <source>BOTTOM-LEFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="configuration.py" line="119"/>
+        <source>All Devices</source>
+        <translation type="unfinished">Tots els dispositius</translation>
+    </message>
+</context>
 <context>
     <name>Dialog</name>
     <message>
+        <location filename="calibration2.ui" line="32"/>
         <source>Dialog</source>
         <translation>Diàleg</translation>
     </message>
     <message>
+        <location filename="calibration2.ui" line="1"/>
         <source>abc</source>
-        <translation>abc</translation>
+        <translation type="obsolete">abc</translation>
     </message>
     <message>
+        <location filename="calibration2.ui" line="73"/>
         <source>Cancel calibration</source>
         <translation>Cancel·la la calibració</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="26"/>
         <source>Configuration</source>
         <translation>Configuració</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="42"/>
         <source>General options</source>
         <translation>Opcions generals</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="48"/>
         <source>Auto connect</source>
         <translation>Auto connecta</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="55"/>
         <source>Activate cursor after calibration</source>
         <translation>Activa el cursor després de la calibració</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="62"/>
         <source>Fullscreen Calibration</source>
         <translation>Calibració a pantalla completa</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="69"/>
         <source>Select allowed devices:</source>
         <translation>Seleccioneu els dispositius permesos:</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="79"/>
         <source>Add connected device</source>
         <translation>Afegiu el dispositiu connectat</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="99"/>
         <source>Remove device</source>
         <translation>Elimineu dispositiu</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="106"/>
         <source>Do calibration after connection</source>
         <translation>Calibra en connectar</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="120"/>
         <source>IR Sensitivity:</source>
         <translation>Sensibilitat IR:</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="128"/>
         <source>Toggles</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="145"/>
         <source>Area 1</source>
         <translation>Àrea 1</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="152"/>
         <source>Area 2</source>
         <translation>Àrea 2</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="159"/>
         <source>Area 3</source>
         <translation>Àrea 3</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="166"/>
         <source>Area 4</source>
         <translation>Àrea 4</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="250"/>
         <source>Left Click</source>
         <translation>Clic esquerre</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="255"/>
         <source>Only Move</source>
         <translation>Només mou</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="260"/>
         <source>Right Click</source>
         <translation>Clic dret</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="265"/>
         <source>Middle Click</source>
         <translation>Clic central</translation>
     </message>
     <message>
+        <location filename="configuration.ui" line="294"/>
         <source>OK</source>
         <translation>D&apos;acord</translation>
     </message>
     <message>
+        <location filename="pbar.ui" line="32"/>
         <source>Progress...</source>
         <translation>En curs ...</translation>
     </message>
     <message>
+        <location filename="pbar.ui" line="48"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -113,34 +179,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="mainwindow.ui" line="32"/>
         <source>MainWindow</source>
         <translation>Finestra Principal</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="157"/>
         <source>Connect</source>
         <translation>Connecta</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="279"/>
         <source>Activate</source>
         <translation>Activa</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="71"/>
         <source>Calibrate</source>
         <translation>Calibra</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="97"/>
         <source>Wiimote Battery level:</source>
         <translation>Nivell de la bateria:</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="203"/>
         <source>Utilization: 0%</source>
         <translation>Ús: 0%</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="169"/>
         <source>Load Calibration</source>
         <translation>Carrega calibració</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="182"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -185,54 +259,67 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Tant la llista d&apos;adreces MAC com el dispositiu selccionat seran desats al fitxer de configuració (usant els qsettings de qt).&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="218"/>
         <source>File</source>
         <translation>Fitxer</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="244"/>
         <source>Help</source>
         <translation>Ajuda</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="234"/>
         <source>Exit</source>
         <translation>Surt</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="239"/>
         <source>Quit</source>
         <translation>Surt</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="249"/>
         <source>Configuration</source>
         <translation>Configuració</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="123"/>
         <source>Connected to </source>
         <translation>Connectat a </translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="187"/>
         <source>Disconnect</source>
         <translation>Desconnecta</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="197"/>
         <source>Error during connection</source>
         <translation>Error en la connexió</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="220"/>
         <source>Utilization: </source>
         <translation>Ús: </translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="231"/>
         <source>Error during Calibration</source>
         <translation>Error en la calibració</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="289"/>
         <source>Deactivate</source>
         <translation>Desactiva</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="304"/>
         <source>The application will remain active (systray).</source>
         <translation>L&apos;aplicació romandrà activa (safata del sistema).</translation>
     </message>
     <message>
+        <location filename="pywhiteboard.py" line="304"/>
         <source>To quit, use file-&gt;quit menu</source>
         <translation>Per sortir, usau el menú Fitxer -&gt; Surt</translation>
     </message>
