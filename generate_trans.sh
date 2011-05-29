2fd89577f5265f35a9c54c569d8b25dda5b4e80c
Pere Negre <pere.negre@gmail.com>
2011-05-29 22:33:59 +0200

    Added french translation

--- generate_trans.sh
+++ generate_trans.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pylupdate4 stuff/*.ui stuff/*.py -ts trans/pywhiteboard_es.ts trans/pywhiteboard_ca.ts trans/pywhiteboard_it.ts
+pylupdate4 stuff/*.ui stuff/*.py -ts trans/pywhiteboard_es.ts trans/pywhiteboard_ca.ts trans/pywhiteboard_it.ts trans/pywhiteboard_fr.ts
 
