e710a72ea22d350ce35354e433912d293e06f4a6
pere.negre <pere.negre@gmail.com>
2014-03-23 01:03:10 +0100

    Added thread lock in enabling/disabling wiimote callback

--- stuff/cursor.py
+++ stuff/cursor.py
@@ -10,241 +10,241 @@ from configuration import Configuration
 
 
 def clock():
-	return int(time.time()*1000)
+    return int(time.time()*1000)
 
 
 class Filter:
-	def __init__(self):
-		self.data = []
-		conf = Configuration()
-		self.limit = int(conf.getValueStr("smoothing"))
-	
-	def update(self,p):
-		self.data.append(p)
-		if len(self.data) > self.limit:
-			self.data.pop(0)
-			
-		n = len(self.data)
-		res = [0,0]
-		for p in self.data:
-			res[0] += p[0]
-			res[1] += p[1]
-		res[0] /= n
-		res[1] /= n
-		return res
+    def __init__(self):
+        self.data = []
+        conf = Configuration()
+        self.limit = int(conf.getValueStr("smoothing"))
+    
+    def update(self,p):
+        self.data.append(p)
+        if len(self.data) > self.limit:
+            self.data.pop(0)
+            
+        n = len(self.data)
+        res = [0,0]
+        for p in self.data:
+            res[0] += p[0]
+            res[1] += p[1]
+        res[0] /= n
+        res[1] /= n
+        return res
 
 class Click:
-	UP_TIMEOUT = 100
-	
-	def __init__(self,cursor):
-		self.initialTime = clock()
-		self.cursor = cursor
-		self.cursor.mouse_up()
-		self.cursor.mouse_down()
-	
-	# Called when needing to update without IR Data
-	def updateWithoutData(self):
-		if (clock() - self.initialTime) > Click.UP_TIMEOUT:
-			self.cursor.mouse_up()
-			return False
-		
-		return True
-	
-	# Update event with IR Data
-	def updateWithData(self):
-		self.initialTime = clock()
+    UP_TIMEOUT = 100
+    
+    def __init__(self,cursor):
+        self.initialTime = clock()
+        self.cursor = cursor
+        self.cursor.mouse_up()
+        self.cursor.mouse_down()
+    
+    # Called when needing to update without IR Data
+    def updateWithoutData(self):
+        if (clock() - self.initialTime) > Click.UP_TIMEOUT:
+            self.cursor.mouse_up()
+            return False
+        
+        return True
+    
+    # Update event with IR Data
+    def updateWithData(self):
+        self.initialTime = clock()
 
 
 class FakeCursor:
-	LEFT_BUTTON = 1
-	MIDDLE_BUTTON = 2
-	RIGHT_BUTTON = 3
-	ONLY_MOVE = 4
-	RIGHT_CLICK_TIMEOUT = 750
-	CLICK_TIMEOUT = 60
-	ZONE1, ZONE2, ZONE3, ZONE4 = range(4)
-	
-	def __init__(self,wii):
-		self.display = Xlib.display.Display()
-		self.screen = self.display.screen()
-		self.wii = wii
-		self.click = None
-		self.filt = None
-		self.clickType = FakeCursor.LEFT_BUTTON
-		self.zones = {}
-		self.mutex = qt.QMutex()
-		self.mustFinish = False
-		self.thread = None
-		self.noClicks = False
-		# Start of click - measure timeout for right click
-		self.begin = None
-		# Keepalive of IR signal
-		self.lastdata = 0
-		# Starts click (left or right) on timeout or move
-		self.run = False
-		# Delay to only move, before click
-		self.toclick = False
-	
-	
-	def setZone(self,zone,clickType):
-		self.zones[zone] = clickType
-	
-	def setZones(self,actions):
-		for z,a in zip((FakeCursor.ZONE1,FakeCursor.ZONE2,FakeCursor.ZONE3,FakeCursor.ZONE4),actions):
-			if a == '2':
-				self.setZone(z,FakeCursor.RIGHT_BUTTON)
-			elif a == '0':
-				self.setZone(z,FakeCursor.LEFT_BUTTON)
-			elif a == '3':
-				self.setZone(z,FakeCursor.MIDDLE_BUTTON)
-			elif a == '1':
-				self.setZone(z,FakeCursor.ONLY_MOVE)
-			else:
-				self.setZone(z,FakeCursor.LEFT_BUTTON)
-	
-	
-	def move(self,pos):
-		Xlib.ext.xtest.fake_input(self.display, Xlib.X.MotionNotify, x=pos[0], y=pos[1])
-		self.display.sync()
-	
-	
-	def mouse_down(self):
-		if self.noClicks or self.clickType == FakeCursor.ONLY_MOVE: 
-			self.clickType = None
-			return
-		button = self.clickType
-		Xlib.ext.xtest.fake_input(self.display, Xlib.X.ButtonPress, button)
-		self.display.sync()
-	
-	
-	def mouse_up(self):
-		if self.clickType is None:
-			return
-		button = self.clickType
-		Xlib.ext.xtest.fake_input(self.display, Xlib.X.ButtonRelease, button)
-		self.display.sync()
-	
-	
-	# Returns True if point is within screen limits
-	def checkLimits(self,pos):
-		if pos[1] < 0:
-			# Zone 1
-			self.clickType = self.zones[FakeCursor.ZONE1]
-			return False
-		if pos[0] > self.screen['width_in_pixels']:
-			# Zone 2
-			self.clickType = self.zones[FakeCursor.ZONE2]
-			return False
-		if pos[1] > self.screen['height_in_pixels']:
-			# Zone 3
-			self.clickType = self.zones[FakeCursor.ZONE3]
-			return False
-		if pos[0] < 0:
-			# Zone 4
-			self.clickType = self.zones[FakeCursor.ZONE4]
-			return False
-		
-		return True
-	
-	
-	# This function fabricates the callback function that is to be
-	# passed to the wiimote object. It's called every time that IR data
-	# is available. It is necessary to do it this way because the callback
-	# has to be aware of the cursor object.
-	def makeCallback(self):
-		def callback(q):
-			self.mutex.lock()
-			if self.checkLimits(q):
-				if not self.filt:
-					self.filt = Filter()
-				p = self.filt.update(q)
-				
-				self.move(p)
-				
-				if self.run:
-					if not self.click:
-						self.click = Click(self)
-					else:
-						self.click.updateWithData()
-				else:
-					if not self.begin:
-						self.begin = clock()
-						self.clkP = p
-					else:
-						self.lastdata = clock()
-						# timeout for "only move" flash
-						if clock() - self.begin > FakeCursor.CLICK_TIMEOUT:
-							self.toclick = True
-						
-						# timeout for "right click"
-						if clock() - self.begin > FakeCursor.RIGHT_CLICK_TIMEOUT:
-							self.begin = None
-							self.run = True
-							self.clickType = FakeCursor.RIGHT_BUTTON
-						else:
-							dX = p[0]-self.clkP[0]
-							dY = p[1]-self.clkP[1]
-							# if the cursor moves, start click (drag) immediatly
-							if (dX*dX)+(dY*dY) > 16:
-								self.begin = None
-								self.run = True
-								# Returns to the originlal point and clicks there
-								self.move(self.clkP)
-								self.click = Click(self)
+    LEFT_BUTTON = 1
+    MIDDLE_BUTTON = 2
+    RIGHT_BUTTON = 3
+    ONLY_MOVE = 4
+    RIGHT_CLICK_TIMEOUT = 750
+    CLICK_TIMEOUT = 60
+    ZONE1, ZONE2, ZONE3, ZONE4 = range(4)
+    
+    def __init__(self,wii):
+        self.display = Xlib.display.Display()
+        self.screen = self.display.screen()
+        self.wii = wii
+        self.click = None
+        self.filt = None
+        self.clickType = FakeCursor.LEFT_BUTTON
+        self.zones = {}
+        self.mutex = qt.QMutex()
+        self.mustFinish = False
+        self.thread = None
+        self.noClicks = False
+        # Start of click - measure timeout for right click
+        self.begin = None
+        # Keepalive of IR signal
+        self.lastdata = 0
+        # Starts click (left or right) on timeout or move
+        self.run = False
+        # Delay to only move, before click
+        self.toclick = False
+    
+    
+    def setZone(self,zone,clickType):
+        self.zones[zone] = clickType
+    
+    def setZones(self,actions):
+        for z,a in zip((FakeCursor.ZONE1,FakeCursor.ZONE2,FakeCursor.ZONE3,FakeCursor.ZONE4),actions):
+            if a == '2':
+                self.setZone(z,FakeCursor.RIGHT_BUTTON)
+            elif a == '0':
+                self.setZone(z,FakeCursor.LEFT_BUTTON)
+            elif a == '3':
+                self.setZone(z,FakeCursor.MIDDLE_BUTTON)
+            elif a == '1':
+                self.setZone(z,FakeCursor.ONLY_MOVE)
+            else:
+                self.setZone(z,FakeCursor.LEFT_BUTTON)
+    
+    
+    def move(self,pos):
+        Xlib.ext.xtest.fake_input(self.display, Xlib.X.MotionNotify, x=pos[0], y=pos[1])
+        self.display.sync()
+    
+    
+    def mouse_down(self):
+        if self.noClicks or self.clickType == FakeCursor.ONLY_MOVE: 
+            self.clickType = None
+            return
+        button = self.clickType
+        Xlib.ext.xtest.fake_input(self.display, Xlib.X.ButtonPress, button)
+        self.display.sync()
+    
+    
+    def mouse_up(self):
+        if self.clickType is None:
+            return
+        button = self.clickType
+        Xlib.ext.xtest.fake_input(self.display, Xlib.X.ButtonRelease, button)
+        self.display.sync()
+    
+    
+    # Returns True if point is within screen limits
+    def checkLimits(self,pos):
+        if pos[1] < 0:
+            # Zone 1
+            self.clickType = self.zones[FakeCursor.ZONE1]
+            return False
+        if pos[0] > self.screen['width_in_pixels']:
+            # Zone 2
+            self.clickType = self.zones[FakeCursor.ZONE2]
+            return False
+        if pos[1] > self.screen['height_in_pixels']:
+            # Zone 3
+            self.clickType = self.zones[FakeCursor.ZONE3]
+            return False
+        if pos[0] < 0:
+            # Zone 4
+            self.clickType = self.zones[FakeCursor.ZONE4]
+            return False
+        
+        return True
+    
+    
+    # This function fabricates the callback function that is to be
+    # passed to the wiimote object. It's called every time that IR data
+    # is available. It is necessary to do it this way because the callback
+    # has to be aware of the cursor object.
+    def makeCallback(self):
+        def callback(q):
+            self.mutex.lock()
+            if self.checkLimits(q):
+                if not self.filt:
+                    self.filt = Filter()
+                p = self.filt.update(q)
+                
+                self.move(p)
+                
+                if self.run:
+                    if not self.click:
+                        self.click = Click(self)
+                    else:
+                        self.click.updateWithData()
+                else:
+                    if not self.begin:
+                        self.begin = clock()
+                        self.clkP = p
+                    else:
+                        self.lastdata = clock()
+                        # timeout for "only move" flash
+                        if clock() - self.begin > FakeCursor.CLICK_TIMEOUT:
+                            self.toclick = True
+                        
+                        # timeout for "right click"
+                        if clock() - self.begin > FakeCursor.RIGHT_CLICK_TIMEOUT:
+                            self.begin = None
+                            self.run = True
+                            self.clickType = FakeCursor.RIGHT_BUTTON
+                        else:
+                            dX = p[0]-self.clkP[0]
+                            dY = p[1]-self.clkP[1]
+                            # if the cursor moves, start click (drag) immediatly
+                            if (dX*dX)+(dY*dY) > 16:
+                                self.begin = None
+                                self.run = True
+                                # Returns to the originlal point and clicks there
+                                self.move(self.clkP)
+                                self.click = Click(self)
 
-			self.mutex.unlock()
-		
-		return callback
-	
-	
-	# This function creates the cursor thread. It makes the callback to the
-	# thread and enables the wiimote device
-	def runThread(self):
-		def runFunc():
-			while 1:
-				qt.QThread.usleep(50)
-				self.mutex.lock()
-				# A click had occourred: a short burst (noot an impulse) with no movement
-				if self.toclick and not self.run and clock() - self.lastdata > Click.UP_TIMEOUT:
-					self.begin = None
-					self.lastdata = None
-					self.toclick = False
-					self.run = True
-					self.click = Click(self)
-					
-				if self.click and self.click.updateWithoutData() == False:
-					self.click = None
-					self.filt = None
-					self.begin = None
-					self.lastdata = None
-					self.run = False
-					self.toclick = False
-					self.clickType = FakeCursor.LEFT_BUTTON
-				if self.mustFinish:
-					self.mutex.unlock()
-					break
-				self.mutex.unlock()
-		
-		from threads import CreateThreadClass
-		
-		self.mustFinish = False
-		self.wii.disable()
-		self.wii.putCallbackIR(self.makeCallback())
-		self.wii.enable()
-		thread = CreateThreadClass(runFunc)
-		self.thread = thread()
-		self.thread.start()
-	
-	
-	# Destroys the cursor thread and disables the wiimote device
-	def finish(self):
-		self.mutex.lock()
-		self.mustFinish = True
-		self.mutex.unlock()
-		self.thread.wait()
-		self.wii.disable()
-		self.wii.putCallbackIR(None)
-		self.thread = None
-		
+            self.mutex.unlock()
+        
+        return callback
+    
+    
+    # This function creates the cursor thread. It makes the callback to the
+    # thread and enables the wiimote device
+    def runThread(self):
+        def runFunc():
+            while 1:
+                qt.QThread.usleep(50)
+                self.mutex.lock()
+                # A click had occourred: a short burst (noot an impulse) with no movement
+                if self.toclick and not self.run and clock() - self.lastdata > Click.UP_TIMEOUT:
+                    self.begin = None
+                    self.lastdata = None
+                    self.toclick = False
+                    self.run = True
+                    self.click = Click(self)
+                    
+                if self.click and self.click.updateWithoutData() == False:
+                    self.click = None
+                    self.filt = None
+                    self.begin = None
+                    self.lastdata = None
+                    self.run = False
+                    self.toclick = False
+                    self.clickType = FakeCursor.LEFT_BUTTON
+                if self.mustFinish:
+                    self.mutex.unlock()
+                    break
+                self.mutex.unlock()
+        
+        from threads import CreateThreadClass
+        
+        self.mustFinish = False
+        self.wii.disable()
+        self.wii.putCallbackIR(self.makeCallback())
+        self.wii.enable()
+        thread = CreateThreadClass(runFunc)
+        self.thread = thread()
+        self.thread.start()
+    
+    
+    # Destroys the cursor thread and disables the wiimote device
+    def finish(self):
+        self.mutex.lock()
+        self.mustFinish = True
+        self.mutex.unlock()
+        self.thread.wait()
+        self.wii.disable()
+        self.wii.putCallbackIR(None)
+        self.thread = None
+        
 
 
--- stuff/wiimote.py
+++ stuff/wiimote.py
@@ -11,30 +11,31 @@ import bluetooth
 import linuxWiimoteLib as wiLib
 from configuration import Configuration
 from threads import CreateThreadClass
+import threading
 
 
 
 def calculateArea(points):
-	print points
-	p1 = points[0]
-	p2 = points[1]
-	p3 = points[2]
-	p4 = points[3]
+    print points
+    p1 = points[0]
+    p2 = points[1]
+    p3 = points[2]
+    p4 = points[3]
 
-	vb = [p2[0]-p1[0], p2[1]-p1[1]]
-	va = [p4[0]-p1[0], p4[1]-p1[1]]
+    vb = [p2[0]-p1[0], p2[1]-p1[1]]
+    va = [p4[0]-p1[0], p4[1]-p1[1]]
 
-	paral_A_area = va[0]*vb[1] - va[1]*vb[0]
+    paral_A_area = va[0]*vb[1] - va[1]*vb[0]
 
-	va = [p2[0]-p3[0], p2[1]-p3[1]]
-	vb = [p4[0]-p3[0], p4[1]-p3[1]]
+    va = [p2[0]-p3[0], p2[1]-p3[1]]
+    vb = [p4[0]-p3[0], p4[1]-p3[1]]
 
-	paral_B_area = va[0]*vb[1] - va[1]*vb[0]
+    paral_B_area = va[0]*vb[1] - va[1]*vb[0]
 
-	result = float(paral_A_area)/2 + float(paral_B_area)/2
-	print paral_A_area
-	print paral_B_area
-	return result
+    result = float(paral_A_area)/2 + float(paral_B_area)/2
+    print paral_A_area
+    print paral_B_area
+    return result
 
 
 
@@ -42,176 +43,186 @@ def calculateArea(points):
 
 
 class Wiimote:
-	CALIBRATED, NONCALIBRATED = range(2)
-	MAX_X = 1023
-	MAX_Y = 768
-	
-	def __init__(self):
-		self.wii = None
-		self.error = False
-		self.pos = None
-		self.state = Wiimote.NONCALIBRATED
-		self.calibrationPoints = []
-		self.screenPoints = []
-		self.utilization = 0.0
-		self.funcIR = None
-		self.funcBTN = None
-		self.enableCallback = False
-	
-	
-	def create_wiimote_callback(self):
-		# Closure
-		def wiimote_callback(px,py):
-			if self.enableCallback and self.funcIR is not None:
-				self.funcIR(self.getPos([px,py]))
-		
-		return wiimote_callback
-	
-	
-	def putCallbackIR(self,funcIR):
-		self.funcIR = funcIR
-	
-	def putCallbackBTN(self,funcBTN):
-		self.funcBTN = funcBTN
-	
-	
-	def detectWiimotes(self):
-		try:
-			self.wiimotesDetected = []
-			devices = bluetooth.discover_devices(duration=10, lookup_names=True)
-			for device in devices:
-				if re.match('.*nintendo.*', device[1].lower()):
-					self.wiimotesDetected.append(device)
-			return
-		
-		except bluetooth.BluetoothError, errString:
-			self.wii = None
-			self.error = True
-			return
-	
-	
-	def bind(self, device):
-		try:
-			self.addr = str(device[0])
-			self.wii = wiLib.Wiimote()
-			self.wii.Connect(device)
-			self.wii.SetRumble(True)
-			qt.QThread.msleep(200)
-			self.wii.SetRumble(False)
-			self.wii.setIRCallBack(self.create_wiimote_callback())
-			self.wii.activate_IR(int(Configuration().getValueStr("sensitivity")))
-			self.wii.SetLEDs(True, False, False, False)
-			self.error = False
-			return
-			
-		except RuntimeError, errString:
-			self.wii = None
-			return
-		
-		except:
-			self.wii = None
-			self.error = True
-			print "Unexpected error:", sys.exc_info()[0]
-			raise
-	
-	def isConnected(self):
-		if self.wii: return True
-		return False
-	
-	def enable(self):
-		self.error = False
-		self.wii.setIRSensitivity(int(Configuration().getValueStr("sensitivity")))
-		self.enableCallback = True
-	
-	def disable(self):
-		self.enableCallback = False
-	
-	def close(self):
-		self.disable()
-		self.wii.Disconnect()
-		self.wii = None
-	
-	def checkStatus(self):
-		return True
-		#if self.wii == None or self.error == True: return False
-		#try:
-			#self.wii.request_status()
-			#return True
-		#except:
-			#return False
-	
-	def battery(self):
-		return self.wii.WiimoteState.Battery 
-	
-	def getPos(self,p):
-		if self.state == Wiimote.NONCALIBRATED:
-			return p
-		if self.state == Wiimote.CALIBRATED:
-			return [
-				(self.h11*p[0] + self.h12*p[1] + self.h13) / \
-				(self.h31*p[0] + self.h32*p[1] + 1),
-				(self.h21*p[0] + self.h22*p[1] + self.h23) / \
-				(self.h31*p[0] + self.h32*p[1] + 1) ]
-	
-	def calibrate(self, p_screen, p_wii):
-		l = []
-		for i in range(0,4):
-			l.append( [p_wii[i][0], p_wii[i][1], 1, 0, 0, 0, 
-				(-p_screen[i][0] * p_wii[i][0]), 
-				(-p_screen[i][0] * p_wii[i][1])] )
-			l.append( [0, 0, 0, p_wii[i][0], p_wii[i][1], 1, 
-				(-p_screen[i][1] * p_wii[i][0]), 
-				(-p_screen[i][1] * p_wii[i][1])] )
+    CALIBRATED, NONCALIBRATED = range(2)
+    MAX_X = 1023
+    MAX_Y = 768
+    
+    def __init__(self):
+        self.wii = None
+        self.error = False
+        self.pos = None
+        self.state = Wiimote.NONCALIBRATED
+        self.calibrationPoints = []
+        self.screenPoints = []
+        self.utilization = 0.0
+        self.funcIR = None
+        self.funcBTN = None
+        self.enableCallback = False
+        self.lock = threading.Lock()
+    
+    
+    def create_wiimote_callback(self):
+        # Closure
+        def wiimote_callback(px,py):
+            self.lock.acquire()
+            if self.enableCallback and self.funcIR is not None:
+                self.lock.release()
+                self.funcIR(self.getPos([px,py]))
+            else:
+                self.lock.release()
+        
+        return wiimote_callback
+    
+    
+    def putCallbackIR(self,funcIR):
+        self.funcIR = funcIR
+    
+    def putCallbackBTN(self,funcBTN):
+        self.funcBTN = funcBTN
+    
+    
+    def detectWiimotes(self):
+        try:
+            self.wiimotesDetected = []
+            devices = bluetooth.discover_devices(duration=10, lookup_names=True)
+            for device in devices:
+                if re.match('.*nintendo.*', device[1].lower()):
+                    self.wiimotesDetected.append(device)
+            return
+        
+        except bluetooth.BluetoothError, errString:
+            self.wii = None
+            self.error = True
+            return
+    
+    
+    def bind(self, device):
+        try:
+            self.addr = str(device[0])
+            self.wii = wiLib.Wiimote()
+            self.wii.Connect(device)
+            self.wii.SetRumble(True)
+            qt.QThread.msleep(200)
+            self.wii.SetRumble(False)
+            self.wii.setIRCallBack(self.create_wiimote_callback())
+            self.wii.activate_IR(int(Configuration().getValueStr("sensitivity")))
+            self.wii.SetLEDs(True, False, False, False)
+            self.error = False
+            return
+            
+        except RuntimeError, errString:
+            self.wii = None
+            return
+        
+        except:
+            self.wii = None
+            self.error = True
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+    
+    def isConnected(self):
+        if self.wii: return True
+        return False
+    
+    def enable(self):
+        self.lock.acquire()
+        self.error = False
+        self.wii.setIRSensitivity(int(Configuration().getValueStr("sensitivity")))
+        self.enableCallback = True
+        self.lock.release()
+    
+    def disable(self):
+        self.lock.acquire()
+        self.enableCallback = False
+        self.lock.release()
+    
+    def close(self):
+        self.disable()
+        self.wii.Disconnect()
+        self.wii = None
+    
+    def checkStatus(self):
+        return True
+        #if self.wii == None or self.error == True: return False
+        #try:
+            #self.wii.request_status()
+            #return True
+        #except:
+            #return False
+    
+    def battery(self):
+        return self.wii.WiimoteState.Battery 
+    
+    def getPos(self,p):
+        if self.state == Wiimote.NONCALIBRATED:
+            return p
+        if self.state == Wiimote.CALIBRATED:
+            return [
+                (self.h11*p[0] + self.h12*p[1] + self.h13) / \
+                (self.h31*p[0] + self.h32*p[1] + 1),
+                (self.h21*p[0] + self.h22*p[1] + self.h23) / \
+                (self.h31*p[0] + self.h32*p[1] + 1) ]
+    
+    def calibrate(self, p_screen, p_wii):
+        l = []
+        for i in range(0,4):
+            l.append( [p_wii[i][0], p_wii[i][1], 1, 0, 0, 0, 
+                (-p_screen[i][0] * p_wii[i][0]), 
+                (-p_screen[i][0] * p_wii[i][1])] )
+            l.append( [0, 0, 0, p_wii[i][0], p_wii[i][1], 1, 
+                (-p_screen[i][1] * p_wii[i][0]), 
+                (-p_screen[i][1] * p_wii[i][1])] )
 
-		A = matrix(l)
+        A = matrix(l)
 
-		x = matrix( [
-			[p_screen[0][0]],
-			[p_screen[0][1]],
-			[p_screen[1][0]],
-			[p_screen[1][1]],
-			[p_screen[2][0]],
-			[p_screen[2][1]],
-			[p_screen[3][0]],
-			[p_screen[3][1]],
-		])
+        x = matrix( [
+            [p_screen[0][0]],
+            [p_screen[0][1]],
+            [p_screen[1][0]],
+            [p_screen[1][1]],
+            [p_screen[2][0]],
+            [p_screen[2][1]],
+            [p_screen[3][0]],
+            [p_screen[3][1]],
+        ])
 
-		self.hCoefs = linalg.solve(A, x)
-		self.h11 = self.hCoefs[0]
-		self.h12 = self.hCoefs[1]
-		self.h13 = self.hCoefs[2]
-		self.h21 = self.hCoefs[3]
-		self.h22 = self.hCoefs[4]
-		self.h23 = self.hCoefs[5]
-		self.h31 = self.hCoefs[6]
-		self.h32 = self.hCoefs[7]
-		
-		self.calibrationPoints = list(p_wii)
-		self.screenPoints = list(p_screen)
-		self.state = Wiimote.CALIBRATED
-		
-		area_inside = calculateArea(self.calibrationPoints)
-		total_area = Wiimote.MAX_X * Wiimote.MAX_Y
-		self.utilization = float(area_inside)/float(total_area)
-	
-	
-	def createConnectThread(self, selectedmac, pool):
-		def func():			
-			if selectedmac == '*':
-			        self.detectWiimotes()
-				if len(self.wiimotesDetected) == 0: return
-				
-				for p in self.wiimotesDetected:
-					if not p[0] in pool:
-						pool.append(p[0])
-				
-			else:
-				mac = str(selectedmac)
-                                name = bluetooth.lookup_name(mac)
-				self.bind([mac,name])
-		
-		thread = CreateThreadClass(func)
-		return thread() 
+        self.hCoefs = linalg.solve(A, x)
+        self.h11 = self.hCoefs[0]
+        self.h12 = self.hCoefs[1]
+        self.h13 = self.hCoefs[2]
+        self.h21 = self.hCoefs[3]
+        self.h22 = self.hCoefs[4]
+        self.h23 = self.hCoefs[5]
+        self.h31 = self.hCoefs[6]
+        self.h32 = self.hCoefs[7]
+        
+        self.calibrationPoints = list(p_wii)
+        self.screenPoints = list(p_screen)
+        self.state = Wiimote.CALIBRATED
+        
+        area_inside = calculateArea(self.calibrationPoints)
+        total_area = Wiimote.MAX_X * Wiimote.MAX_Y
+        self.utilization = float(area_inside)/float(total_area)
+    
+    
+    def createConnectThread(self, selectedmac, pool):
+        def func():         
+            if selectedmac == '*':
+                self.detectWiimotes()
+                if len(self.wiimotesDetected) == 0:
+                    return
+                
+                for p in self.wiimotesDetected:
+                    if not p[0] in pool:
+                        pool.append(p[0])
+                
+            else:
+                mac = str(selectedmac)
+                name = bluetooth.lookup_name(mac)
+                self.bind([mac,name])
+        
+        thread = CreateThreadClass(func)
+        return thread() 
 
 
 
